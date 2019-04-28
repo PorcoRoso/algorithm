84cb20710d601ba0a55d4b96356cb86afaefa68c
PorcoRoso <wzycumt@yeah.net>
2019-04-28 19:14:39 +0800

    4月份LeetCode提交

--- dynamicPrograming/DP_test.go
+++ dynamicPrograming/DP_test.go
@@ -5,9 +5,20 @@ import "testing"
 func TestMinCostClimbingStairs(t *testing.T) {
 	cost := []int{1, 100, 1, 1, 1, 100, 1, 1, 100, 1}
 	res := minCostClimbingStairs(cost)
-	if res == 15 {
+	if res == 6 {
 		t.Log("pass")
 	} else {
 		t.Errorf("error res: %v", res)
+	}
+}
+
+func TestWordBreak(t *testing.T) {
+	s := "catsand"
+	wordDict := []string{"cats", "and"}
+	res := wordBreak(s, wordDict)
+	if res {
+		t.Log("pass")
+	} else {
+		t.Errorf("error")
 	}
 }
--- dynamicPrograming/wordBreak.go
+++ dynamicPrograming/wordBreak.go
@@ -1,1 +1,26 @@
 package dynamicPrograming
+
+func isContain(s string, wordDoct []string) bool {
+	for _, v := range wordDoct {
+		if s == v {
+			return true
+		}
+	}
+	return false
+}
+
+// catsand, ["cats", "and"]
+// dp[7] = dp[6] && s[6:7], dp[5] && s[5:7], ..., dp[0] && s[0:7]中任一位true，则dp[7]为true
+func wordBreak(s string, wordDict []string) bool {
+	dp := make([]bool, len(s)+1)
+	dp[0] = true
+	for i := 1; i <= len(s); i++ {
+		for j := 0; j < i; j++ {
+			dp[i] = dp[j] && isContain(s[j:i], wordDict)
+			if dp[i] { // 只要存在一个，即表示满足条件
+				break
+			}
+		}
+	}
+	return dp[len(s)]
+}
